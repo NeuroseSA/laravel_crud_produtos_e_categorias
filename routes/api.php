f6f567231914a90b5227d39dad0f21dc86d5213f
neurosesa <alexjunior01221@gmail.com>
2021-01-16 13:55:00 -0400

    Api route authentication.

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/* 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+ */
+
+Route::group(['middleware' => ['ApiJwt']], function(){
+    Route::get('users', 'Api\\UserController@index');
+});
+
+Route::post('auth/login', 'Api\\AuthController@login');
+
 
 Route::get('/categorias', 'CategoryController@indexJson');
 Route::get('/produtos', 'ProductController@indexAPI');
